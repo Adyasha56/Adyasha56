a3957be92c2a48eb46bbac2d7e43bf7a99e0c3bc
Adyasha Nanda <136905764+Adyasha56@users.noreply.github.com>
2024-09-07 14:37:44 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,12 +1,8 @@
-- 👋 Hi, I’m @Adyasha56
-- 🌱 I’m currently learning full stack webdevelopement
-- 💞️ I’m looking to collaborate on projects based on webdeveloping
-- 📫 How to reach me 
-email:adyashananda105@gmail.com
+ 👋 Hi, I’m @Adyasha56
+ I’m currently learning MERN-stack webdevelopement
+ I’m looking to collaborate on projects based on webdeveloping
+ How to reach me :adyashananda105@gmail.com
 
                           
 
-<!---
-Adyasha56/Adyasha56 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+
