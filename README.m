1508bd790f9f1f09b25edd5ba6ea6d48b2f80aa2
Adyasha Nanda <136905764+Adyasha56@users.noreply.github.com>
2024-10-30 23:38:27 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,6 @@
  👋 Hi, I’m @Adyasha56
  I’m currently learning MERN-stack webdevelopement
- I’m looking to collaborate on projects based on webdeveloping
+ I’m looking to collaborate on projects based on webdevelopment.
  How to reach me :adyashananda105@gmail.com
 
                           
