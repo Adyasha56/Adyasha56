1fb2eda131e68c19b94ac3fb87549918d0fb114c
Adyasha Nanda <136905764+Adyasha56@users.noreply.github.com>
2025-05-02 12:16:38 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,20 +1,4 @@
-## 👋 Hi there, I'm Adyasha (@Adyasha56)
-
-### 🚀 About Me
-
-I'm currently learning **MERN stack** and **Next.js** development. I love building websites and turning cool ideas into real projects.
-
-### 💻 What I’m Learning
-
-* **MongoDB, Express.js, React.js, Node.js**
-* **Next.js** for building full-stack apps with ease
-* Creating clean UIs and responsive websites
-
-### 🤝 Looking to Collaborate On
-
-* **Web development projects**
-* Team-based coding challenges or hackathons
-
-### 📫 How to Reach Me
-
-* 📧 **Email**: [nandaadyasha71@gmail.com](mailto:nandaadyasha71@gmail.com
+ 👋 Hi, I’m @Adyasha56
+ I’m currently learning MERN-stack webdevelopement
+ I’m looking to collaborate on projects based on webdevelopment.
+ How to reach me :adyashananda105@gmail.com
